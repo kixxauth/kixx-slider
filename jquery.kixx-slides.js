c6763ef6c06554d673215523885ee46d9b14bf67
Kristoffer Walker <kris@kixx.name>
2013-11-17 07:39:53 -0500

    Functional previous and next buttons.
     On branch master
    
    	modified:   examples.html
    	modified:   jquery.kixx-slides.js

--- jquery.kixx-slides.js
+++ jquery.kixx-slides.js
@@ -35,10 +35,108 @@
       })
     },
 
-    previous: function () {
+    previous: function (opts) {
+      opts = opts || {};
+
+      var self = this
+        , width = self.currentWidth()
+        , inOpts = $.extend({}, opts)
+        , outProps = opts.outProps || {left: width + 1}
+        , inProps = opts.inProps || {left: 0}
+        , complete = refunct(opts, 'complete')
+        , $next = this.$previous()
+        , hideDone = false
+        , showDone = true
+
+      this.containSlide($next.show()).css({
+        position: 'absolute'
+      , top: 0
+      , left: -(width - 1)
+      , width: width
+      , height: self.currentHeight()
+      });
+
+      function maybeComplete(done) {
+        if (done.show) {
+          showDone = true;
+        } else {
+          hideDone = true;
+        }
+
+        if (hideDone && showDone) {
+          self.$current = $next;
+          complete();
+        }
+      }
+
+      opts.complete = function () {
+        self.$current.hide();
+        maybeComplete({hide: true});
+      };
+
+      inOpts.complete = function () {
+        maybeComplete({show: true});
+      };
+
+      if (this.$current) {
+        this.$current.animate(outProps, opts);
+      } else {
+        maybeComplete({hide: true});
+      }
+
+      $next.animate(inProps, inOpts);
     },
 
-    next: function () {
+    next: function (opts) {
+      opts = opts || {};
+
+      var self = this
+        , width = self.currentWidth()
+        , inOpts = $.extend({}, opts)
+        , outProps = opts.outProps || {left: -(width - 1)}
+        , inProps = opts.inProps || {left: 0}
+        , complete = refunct(opts, 'complete')
+        , $next = this.$next()
+        , hideDone = false
+        , showDone = true
+
+      this.containSlide($next.show()).css({
+        position: 'absolute'
+      , top: 0
+      , left: width + 1
+      , width: width
+      , height: self.currentHeight()
+      });
+
+      function maybeComplete(done) {
+        if (done.show) {
+          showDone = true;
+        } else {
+          hideDone = true;
+        }
+
+        if (hideDone && showDone) {
+          self.$current = $next;
+          complete();
+        }
+      }
+
+      opts.complete = function () {
+        self.$current.hide();
+        maybeComplete({hide: true});
+      };
+
+      inOpts.complete = function () {
+        maybeComplete({show: true});
+      };
+
+      if (this.$current) {
+        this.$current.animate(outProps, opts);
+      } else {
+        maybeComplete({hide: true});
+      }
+
+      $next.animate(inProps, inOpts);
     },
 
     show: function (id, opts) {
@@ -68,6 +166,22 @@
       }
     },
 
+    $next: function () {
+      var i = this.$slides.filter(this.$current).index()
+        , next = i + 1
+
+      if (next >= this.$slides.length) return this.$slides.first();
+      return $(this.$slides[next]);
+    },
+
+    $previous: function () {
+      var i = this.$slides.filter(this.$current).index()
+        , next = i - 1
+
+      if (next < 0) return this.$slides.last();
+      return $(this.$slides[next]);
+    },
+
     currentWidth: function () {
       return this.$container.innerWidth();
     },
@@ -86,18 +200,13 @@
         , marginLeft = 0
         , marginTop = 0
 
-      console.log('AR', ar)
       if ((w - width) > 0 || (h - height) > 0) {
         if (w/width < h/height) {
-          console.log('match height', height, h)
           h = height;
           w = h * ar;
-          console.log('width', w)
         } else {
-          console.log('match width', width, w)
           w = width;
           h = w / ar;
-          console.log('height', h)
         }
       }
 
@@ -107,15 +216,6 @@
       if (h < height) {
         marginTop = (height - h) / 2;
       }
-
-      console.log($inner.length)
-      console.log({
-        display: 'block'
-      , width: w
-      , height: h
-      , marginTop: marginTop
-      , marginLeft: marginLeft
-      })
 
       $inner.css({
         display: 'block'
@@ -133,11 +233,13 @@
     return str.replace(/^#/, '');
   };
 
+  kixxSlides.noop = function () {};
+
   function refunct(obj, name) {
     if (arguments.length < 2) {
-      return $.isFunction(obj) ? obj : kixxModal.noop;
+      return $.isFunction(obj) ? obj : kixxSlides.noop;
     }
-    return $.isFunction(obj[name]) ? obj[name] : kixxModal.noop;
+    return $.isFunction(obj[name]) ? obj[name] : kixxSlides.noop;
   }
 
   kixxSlides.KixxSlides = KixxSlides;
